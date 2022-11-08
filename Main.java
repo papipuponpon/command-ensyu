2826e0ea43f5fa7902502f2cba0903ecc5c683f2
kudou <h.kudou.sys22@morijyobi.ac.jp>
2022-11-08 10:52:54 +0900

    Main.javaðC³

--- Main.java
+++ Main.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) {
-                Scanner sc1 = new Scanner(System.in);
-                System.out.println("身長:");
-                double a = sc1.nextDouble();
-                Scanner sc2 = new Scanner(System.in);
-                System.out.println("体重:");
-                double b = sc2.nextDouble();
-                double bmi=(b/(a*a));
-                System.out.println(String.format("%.2f",bmi));
+		for(int i = 0; i < 2; i++){
+                	Scanner sc1 = new Scanner(System.in);
+                	System.out.println("身長:");
+                	double a = sc1.nextDouble();
+                	Scanner sc2 = new Scanner(System.in);
+                	System.out.println("体重:");
+                	double b = sc2.nextDouble();
+                	double bmi=(b/(a*a));
+                	System.out.println(String.format("%.2f",bmi));
+		}
         }
 }
